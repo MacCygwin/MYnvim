970ea178373d96e50f557c4a607e3021f8f99e03
MacCygwin <mdyusufpool@gmail.com>
2026-06-11 20:40:24 +0800

    Update

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
+vim.wo.number = true  -- Enable absolute line numbers
+vim.wo.relativenumber = true -- Enable relative line numbers
+vim.diagnostic.enable = true
+vim.diagnostic.config({
+  virtual_lines = true,
+})
+vim.opt.clipboard = "unnamedplus"
